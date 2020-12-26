e1b0810e906fd8df729cee9e74368e4fbd6c74fd
Ranvus <mr.random258258@gmail.com>
2020-12-26 21:24:57 +0600

    Spawner prefab

---
+++ Assets/Scripts/SpawnerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerController : MonoBehaviour
+{
+    [SerializeField] private ParticleSystem particles;
+
+    private float timer;
+    private float cooldown = 3f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (timer > 0)
+        {
+            timer -= Time.deltaTime;
+        }
+        else
+        {
+            Shoot();
+            timer = cooldown;
+        }
+    }
+
+    private void Shoot()
+    {
+        particles.Play();
+    }
+}
